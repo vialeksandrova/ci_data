2f91f22a06b29e7813a779d038cf40794c2fd5eb
vi aleksandrova <viialeksandrova@gmail.com>
2014-05-25 22:17:03 +0300

    Create data_model.php

---
+++ models/data_model.php
@@ -0,0 +1,12 @@
+<?php
+class data_model extends CI_Model{
+	function getAll(){
+	$q=$this->db->query("SELECT * FROM data");
+	if($q->num_rows()>0){
+	foreach($q->result() as $row){
+	$data[]=$row;
+	}
+	return $data;
+	} 
+	}
+}
